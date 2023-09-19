6486350fcac1a853a79c4c5c3ae25135a805b463
Julian Haldenby <j.haldenby@gmail.com>
2023-09-19 18:50:22 -0400

    Fixing the controller machine type

--- install/terraform/modules/gke/cluster.tf
+++ install/terraform/modules/gke/cluster.tf
@@ -27,7 +27,7 @@ locals {
   zone                          = lookup(var.cluster, "zone", "")
   name                          = lookup(var.cluster, "name", "test-cluster")
   machineType                   = lookup(var.cluster, "machineType", "e2-standard-4")
-  controllerMachineType         = lookup(var.cluster, "machineType", "e2-standard-4")
+  controllerMachineType         = lookup(var.cluster, "controllerMachineType", "e2-standard-4")
   initialNodeCount              = lookup(var.cluster, "initialNodeCount", "4")
   enableImageStreaming          = lookup(var.cluster, "enableImageStreaming", true)
   network                       = lookup(var.cluster, "network", "default")
